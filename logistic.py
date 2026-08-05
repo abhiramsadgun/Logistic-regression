b4bff44c3d81bc947b579a64fae2446c2431ba6c
LATTALA ABHIRAM SADGUN <abhiramsadgun94@gmail.com>
2026-08-05 12:29:55 +0530

    Add files via upload

--- logistic.py
+++ logistic.py
@@ -26,6 +26,30 @@ from sklearn.metrics import confusion_matrix,accuracy_score,classification_repor
 
 score=accuracy_score(y_test,y_ped)
 print(score)
-cm=confusion_matrix(y_ped,y_test)
+cm=confusion_matrix(y_test,y_ped)
+print(cm)
+print(classification_report(y_test,y_ped))
+
+model=LogisticRegression()
+c_values=[100,10,1,0.1,0.01]
+param=[
+    {'l1_ratio': [0.0, 1.0], 'C': c_values, 'solver': ['liblinear']},
+    {'l1_ratio': [0.0, 0.5, 1.0], 'C': c_values, 'solver': ['saga']},
+    {'l1_ratio': [0.0], 'C': c_values, 'solver': ['lbfgs', 'newton-cg', 'sag']}
+]
+#CV->cross validation we use GridSearch
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import StratifiedKFold
+cv=StratifiedKFold()
+gsv=GridSearchCV(estimator=model,param_grid=param,scoring='accuracy',cv=cv,n_jobs=-1)
+print(gsv)
+gsv.fit(x_train,y_train)
+print(gsv.best_params_)
+print(gsv.best_score_)
+y_ped=gsv.predict(x_test)
+
+score=accuracy_score(y_test,y_ped)
+print(score)
+cm=confusion_matrix(y_test,y_ped)
 print(cm)
 print(classification_report(y_test,y_ped))
